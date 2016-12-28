c126dbbbfd588031525b2f0cb0feddac45ee55fd
GongT <gongteng524702837@gmail.com>
2016-12-28 15:13:35 +0800

    init

---
+++ .micro-build/config.ts
@@ -0,0 +1,32 @@
+import {MicroBuildConfig, EPlugins} from "./x/microbuild-config";
+declare const build: MicroBuildConfig;
+/*
+ +==================================+
+ | <**DON'T EDIT ABOVE THIS LINE**> |
+ | THIS IS A PLAIN JAVASCRIPT FILE  |
+ |   NOT A TYPESCRIPT OR ES6 FILE   |
+ |    ES6 FEATURES NOT AVAILABLE    |
+ +==================================+
+ */
+
+const projectName = 'privoxy-cn';
+
+build.baseImage('alpine', 'latest');
+build.projectName(projectName);
+build.domainName(projectName + '.gongt');
+
+build.noDataCopy();
+build.systemInstallMethod('apk');
+build.systemInstall('privoxy');
+
+build.startupCommand('--no-daemon', './privoxy/config');
+build.shellCommand('/usr/sbin/privoxy');
+// build.stopCommand('stop.sh');
+
+build.forwardPort(8080, 'tcp').publish(8080);
+
+build.disablePlugin(EPlugins.jenv);
+
+build.volume('./privoxy', '/etc/privoxy');
+
+build.dependService('shadowsocks-client', 'http://github.com/GongT/shadowsocks-client.git');
